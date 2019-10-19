090a56f4337ff9457310dadc55c903e0ebc1f838
Yevhen Chernenko <chernenko.eu@gmail.com>
2019-10-19 23:01:28 +0300

    Create react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App({
+               value,
+               onIncrement,
+               onDecrement
+             }) {
+  return (
+      <div>
+        <h1>{value}</h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+      </div>
+  );
+}
+
+export default App;
